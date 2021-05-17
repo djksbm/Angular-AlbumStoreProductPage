b5035f9c6b1d0fb88b3327399f0260be51aed3cc
djksbm <48785200+djksbm@users.noreply.github.com>
2021-05-17 14:30:16 +0200

    Another typo fix

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,7 @@ export class ProductService {
   private _albumUrl= '../assets/album.json'
   constructor(private _http: Http) { }
 
-  getAlbum(id:number) {
+  getAlbum (id: number) {
     return this._http.get(this._albumUrl).map((response)=>{
       return response.json();
     })
